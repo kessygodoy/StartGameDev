1d2d00f5046c33fcb4aa5f38329f647eb90a4798
kessygod <kessyon@gmail.com>
2021-11-04 19:19:45 -0300

    Implementado metodo Run

--- Projeto/Assets/Scripts/Player.cs
+++ Projeto/Assets/Scripts/Player.cs
@@ -5,22 +5,70 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-    public float speed;
+    [SerializeField] private float speed;
+
+    [SerializeField] private float runSpeed;
 
     private Rigidbody2D rig;
-    private Vector2 direction;
+
+    private float initialSpeed;
+    private bool _isRunning;
+    private Vector2 _direction;
+    
+
+    public Vector2 direction //Encapsulamento para acessar a _direction indiretamente;
+    {
+        get { return _direction;  }
+        set { _direction = value; }
+    }
+        public bool isRunning
+    { 
+        get { return _isRunning;  }
+        set { _isRunning = value; }
+    }
 
     private void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        initialSpeed = speed;
     }
     private void Update()
     {
-        direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        OnInput();
+
+        OnRun();
     }
 
     private void FixedUpdate()
     {
-        rig.MovePosition(rig.position + direction * speed * Time.deltaTime);
+        OnMove();
     }
+
+    #region Movement
+    void OnInput()
+    {
+        _direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+    private void OnMove()
+    {
+        rig.MovePosition(rig.position + _direction * speed * Time.deltaTime); // Rigidbody2D.MovePosition -> move o objeto para a direção indicada;
+    }
+
+    void OnRun()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed = runSpeed;
+            _isRunning = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed = initialSpeed;
+            _isRunning = false;
+        }
+    }
+
+    #endregion
+
 }
